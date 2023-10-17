73d1c97e77360793bb9f6fd31a2311663f5b556b
Nalam Guna Sri Krishna <102983513+GunaNalam@users.noreply.github.com>
2023-10-17 13:09:07 +0530

    Update tic_tac_toe.js

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,15 +1,21 @@
-let tempx=[0,0,0,0,0,0,0,0,0],tempo=[0,0,0,0,0,0,0,0,0]
+let tempx=[0,0,0,0,0,0,0,0,0,0,0,0,0],tempo=[0,0,0,0,0,0,0,0,0,0,0,0,0]
+/*
+4 2 12
+4 0 11
+4 6 9
+4 8 10
+*/
 function check(c)
 {
     let result=0;
     if (c%2)
     {
-        result= (tempx[0] && ((tempx[1] && tempx[2]) || (tempx[4] && tempx[8]) || (tempx[3] && tempx[6]))) || (tempx[4] && ((tempx[2] && tempx[6]) || (tempx[1] && tempx[7]) || (tempx[3] && tempx[5]))) || (tempx[8] && ((tempx[2] && tempx[5]) || (tempx[6] && tempx[7])));
+        result= (tempx[0] && ((tempx[1] && tempx[2]) || (tempx[4] && tempx[8]) || (tempx[3] && tempx[6]))) || (tempx[4] && ((tempx[2] && tempx[6]) || (tempx[1] && tempx[7]) || (tempx[3] && tempx[5]))) || (tempx[8] && ((tempx[2] && tempx[5]) || (tempx[6] && tempx[7]))) || (tempx[4] && ((tempx[0] && tempx[11]) || (tempx[8] && tempx[10]) ||  (tempx[2] && tempx[12]) || (tempx[6] && tempx[9]))) 
         if (result) return 'X';
     }
     else 
     {
-        result=(tempo[0] && ((tempo[1] && tempo[2]) || (tempo[4] && tempo[8]) || (tempo[3] && tempo[6]))) || (tempo[4] && ((tempo[2] && tempo[6]) || (tempo[1] && tempo[7]) || (tempo[3] && tempo[5]))) || (tempo[8] && ((tempo[2] && tempo[5]) || (tempo[6] && tempo[7])));
+        result= (tempo[0] && ((tempo[1] && tempo[2]) || (tempo[4] && tempo[8]) || (tempo[3] && tempo[6]))) || (tempo[4] && ((tempo[2] && tempo[6]) || (tempo[1] && tempo[7]) || (tempo[3] && tempo[5]))) || (tempo[8] && ((tempo[2] && tempo[5]) || (tempo[6] && tempo[7]))) || (tempo[4] && ((tempo[0] && tempo[11]) || (tempo[8] && tempo[10]) ||  (tempo[2] && tempo[12]) || (tempo[6] && tempo[9]))) 
         if (result) return 'O';
     }
     return 'Z'
@@ -28,7 +34,7 @@ function call(e,number)
             document.getElementById('Winner').innerHTML=`${hold} is the Winner`;
             setTimeout(() => {
                 location.reload();
-            },1000);
+            },1500);
         }
         else if (c==9)
         {
@@ -36,7 +42,7 @@ function call(e,number)
             setTimeout(() => {
                 c=0;
                 location.reload();
-            }, 1000);
+            }, 1500);
         }
     }
 }
